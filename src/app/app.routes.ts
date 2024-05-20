e0b129a848db7ae5e842ebaf5916033c6e5eed2d
ESX1234 <ESX1234@DESKTOP-KUP00MC>
2024-05-21 01:42:55 +0200

    Inital Commit Project Structure and Login Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {AuthComponent} from "./components/pages/auth/auth.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthComponent
+  },
+  {
+    path: '', redirectTo: 'auth', pathMatch: 'full',
+  }
+];
