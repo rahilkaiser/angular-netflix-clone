1e15aee33a41afbcfc8266a13a1a618c76610455
ESX1234 <ESX1234@DESKTOP-KUP00MC>
2024-05-23 05:55:29 +0200

    hide nav in auth and register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {NavigationStart, Router, RouterOutlet} from '@angular/router';
 import {HeaderComponent} from "./components/pages/home/components/header/header.component";
+import {AuthService} from "./services/auth.service";
+import {User} from "./models/user.model";
+import {AuthComponent} from "./components/pages/auth/auth.component";
+import {NgIf} from "@angular/common";
+import {RegisterComponent} from "./components/pages/auth/register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-netflix-clone';
 
+  isShow = false;
+
+  constructor(private router: Router) {
+  }
+
+  showHideNav($event: any) {
+    this.isShow = !(($event instanceof AuthComponent) || $event instanceof RegisterComponent);
+  }
 }
